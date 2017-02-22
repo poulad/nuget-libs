c0b2d88ba6b7ac53002a8f6a62966d264974161e
Poulad Ashrafpour <pouladashraf.pour@rbc.com>
2017-02-22 11:59:09 -0500

    Add VS2015 client projects

---
+++ Visual Studio 2015/ClientApplications/WindowsApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Testy.Greeting;
+
+namespace WindowsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = ".NET 4.6";
+
+            var greeter = new WindowsGreeter();
+            greeter.Greet();
+        }
+    }
+}
